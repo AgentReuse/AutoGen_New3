57ca80ed90c87d0cbe742e59ba7203624fbe487f
wrh <wangrhofficial@gmail.com>
2025-07-22 22:04:01 +0800

    删除agent_selector_chat中的计划复用，修复不能运行的问题

--- python/samples/agentchat_plan/agent_selector_chat.py
+++ python/samples/agentchat_plan/agent_selector_chat.py
@@ -1,5 +1,5 @@
 from typing import List, Sequence, cast
-from Response_reuse import SemanticCache
+
 import chainlit as cl
 import yaml
 
@@ -8,18 +8,14 @@ from autogen_agentchat.teams import SelectorGroupChat
 from autogen_agentchat.messages import TextMessage, ModelClientStreamingChunkEvent, BaseAgentEvent, BaseChatMessage
 from autogen_core.models import ChatCompletionClient
 from autogen_core import CancellationToken
-from autogen_agentchat.base import Response
+
+# Example usage in another script:
+from transit_intent import load_models, predict
 
 import os
 
 os.environ['HTTP_PROXY'] = 'http://127.0.0.1:7897'
 os.environ['HTTPS_PROXY'] = 'http://127.0.0.1:7897'
-
-#初始化
-semantic_cache = SemanticCache(
-    embedding_model_path="./m3e-small",
-    cache_path="./semantic_cache"
-)
 
 @cl.step(type="tool")
 async def search_web(query: str) -> str:
@@ -47,7 +43,6 @@ async def start_chat() -> None:
     with open("model_config.yaml", "r") as f:
         model_cfg = yaml.safe_load(f)
     model_client = ChatCompletionClient.load_component(model_cfg)
-
 
     input_refiner = AssistantAgent(
         name="InputRefiner",
@@ -92,8 +87,7 @@ async def start_chat() -> None:
         max_turns=6,
     )
 
-
-
+    cl.user_session.set("input_refiner", input_refiner)
     cl.user_session.set("team", team)  # type: ignore
 
 
@@ -118,39 +112,50 @@ async def set_starts() -> List[cl.Starter]:
 @cl.on_message
 async def chat(message: cl.Message) -> None:
     user_text = message.content
+
+    input_refiner = cl.user_session.get("input_refiner")
+    refined = ""
+    async for evt in input_refiner.on_messages_stream(
+            messages=[TextMessage(content=user_text, source="user")],
+            cancellation_token=CancellationToken(),
+    ):
+        if isinstance(evt, ModelClientStreamingChunkEvent):
+            refined += evt.content
+
+    team: SelectorGroupChat = cl.user_session.get("team")
+    msg = cl.Message(content="")
+
     team = cast(SelectorGroupChat, cl.user_session.get("team"))
 
+    # load_models()  # optional, uses default dirs
+    load_models(intent_dir="transit_intent/bert_intent_model",
+                slot_dir="transit_intent/bert_slot_model")
+    intent = predict(user_text)
+    print(intent)
+
     async for evt in team.run_stream(
-        task=user_text,
+        task=refined,
         cancellation_token=CancellationToken(),
     ):
-        embedding=semantic_cache.get_embedding(user_text)
-        similar_question,score=semantic_cache.search_similar_query(embedding)
-        isReuse =0   #0表示不复用，1表示计划复用，2表示响应复用
-        if score<0.75 :
-            isReuse=0
-        elif 0.75 <= score < 0.90:
-            isReuse=1
-        else:
-            isReuse=2
+        isReuse = 0 ## 0为不复用，1为计划复用，2为响应复用
 
         if isReuse == 0:
             agent_name = getattr(evt, "source", None) or getattr(getattr(evt, "chat_message", None), "source", None)
 
             if agent_name == "InputRefiner":
+                print("InputRefiner has been selected.")
                 if hasattr(evt, "content") and isinstance(evt.content, str):
-                    semantic_cache.save_to_cache(user_text,evt.content,None)   #存储响应
+                    with open("input_refiner.txt", "a", encoding="utf-8") as f:
+                        f.write(evt.content)
 
         elif isReuse == 1:
-            external_content=semantic_cache.cache[user_text]["plan"]    #读取计划
-            ## external_content = "【这是我希望 InputRefiner 说的话，由我外部指定】"
+            external_content = "【这是我希望 InputRefiner 说的话，由我外部指定】"
             msg = TextMessage(source="InputRefiner", content=external_content)
-            team._group_chat_manager._message_thread.append(msg)
+            # team._group_chat_manager._message_thread.append(msg)
             team._group_chat_manager.update_message_thread(msg)
 
         elif isReuse == 2:
-            msg=semantic_cache.cache[user_text]["response"]   #读取响应
-            team._group_chat_manager.update_message_thread(msg)
+            pass
 
         if agent_name == "OutputSummarizer":
             if msg is None:
